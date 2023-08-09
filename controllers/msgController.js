62c4f843cd278e62a2cd868a4a1f976669db33db
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-08-09 18:07:22 +0530

    Added getmsgs

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -21,6 +21,22 @@ const createMsg = async (req, res) => {
   }
 };
 
-const getMsgs = async (req, res) => {};
+const getMsgs = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      return res.status(404).json({ message: "Chat not found" });
+    }
+
+    const messages = await MessageModel.find({ chatId });
+
+    res.status(200).json({ messages });
+  } catch (error) {
+    console.error("Error fetching chat messages:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = { createMsg, getMsgs };
