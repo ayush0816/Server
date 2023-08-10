3737e88c76c8bf079c1735d7ea007054aabd013d
ayush0816 <75437420+ayush0816@users.noreply.github.com>
2023-08-10 17:47:11 +0530

    Configured socket.io

--- index.js
+++ index.js
@@ -22,29 +22,28 @@ initialize(httpserver, {
 });
 
 const io = getIO();
-// var chatRooms = {};
+var chatRooms = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
   socket.on("joinRoom", (chatID) => {
     socket.join(chatID); // Join the specified chat room
-    // if (!chatRooms[chatID]) {
-    //   chatRooms[chatID] = [];
-    // }
-    // console.log("chatRooms: ", chatRooms)
-    // socket.emit("allMessages", chatRooms[chatID]); // Send existing messages to the joining user
+    if (!chatRooms[chatID]) {
+      chatRooms[chatID] = [];
+    }
+    console.log("chatRooms: ", chatRooms);
+    socket.emit("allMessages", chatRooms[chatID]); // Send existing messages to the joining user
   });
 
   socket.on("message", (data) => {
     const { chatID, message } = data;
-    // if (chatRooms[chatID]) {
-    //   chatRooms[chatID].push(message);
-    //   console.log(chatID,message);
-  // }
-      io.to(chatID).emit("newMessage", message); // Emit the new message to everyone in the chat room
+    if (chatRooms[chatID]) {
+      chatRooms[chatID].push(message);
+      console.log(chatID, message);
     }
-  );
+    io.to(chatID).emit("newMessage", message); // Emit the new message to everyone in the chat room
+  });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
